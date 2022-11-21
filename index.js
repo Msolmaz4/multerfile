41c3f5026d73486263035c969419b9b52b36b9de
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-11-21 23:27:38 +0100

    multer1

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 const Port = 3400
+const multer = require('multer')
+const storage= multer.diskStorage({
+    destination:(req,file,cb)=>{
+        //ilk deger hata genlde null yapariz
+        cb(null,'images')
+    },
+    filename:(req,file,cb)=>{
+        console.log(file)
+    }
+})
+
 
 
 
